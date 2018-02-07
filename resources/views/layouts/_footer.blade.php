6abb243a1911a9ed7980c5e4ae92e200363a99f7
Lester Li <Lester.shiyi.li@gmail.com>
2018-02-07 02:28:49 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="" target="_blank">Lester</a> design&code <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">contact us</a></p>
+    </div>
+</footer>
